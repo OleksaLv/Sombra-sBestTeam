1afb51bdca978dfd033c75ad78cd91d21a280f06
IvanOlliinyk <144712641+IvanOlliinyk@users.noreply.github.com>
2024-11-18 21:50:27 +0200

    Update Header.js
    Name changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header style={styles.header}>
-      <h1>My dream team</h1>
+      <h1>Our dream team</h1>
     </header>
   );
 };
