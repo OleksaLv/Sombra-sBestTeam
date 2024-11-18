29cb428bd17cf771148613f1769b2ee9f0159886
Denys Losiev <161693653+DeNforChik@users.noreply.github.com>
2024-11-18 21:04:02 +0200

    Add Denys

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,6 +10,14 @@ const Body = () => {
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
+
+<div style={styles.card}>
+        <img src={"/Denys.png"} alt="Denys Losiev" style={styles.photo} />
+        <div style={styles.textContainer}>
+          <h2 style={styles.name}>Denys Losiev</h2>
+          <p style={styles.position}>Backend developerr</p>
+        </div>
+      </div>
     </main>
   );
 };
